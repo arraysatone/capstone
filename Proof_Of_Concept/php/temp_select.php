73209a91524b4cdce491697f32d4a476590f7534
mharquail <43412943+mharquail@users.noreply.github.com>
2018-10-11 14:20:24 -0400

    Added dynamic max and min temps

--- Proof_Of_Concept/php/temp_select.php
+++ Proof_Of_Concept/php/temp_select.php
@@ -4,7 +4,7 @@ $username = "harquaim_php";
 $password = "c@pstone_server";
 $dbname = "harquaim_capstone";
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
@@ -12,9 +12,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT temp,time, uid FROM Sensor WHERE time=(SELECT MAX(time) FROM Sensor)";
+$sql = "SELECT temp, time, uid FROM Sensor WHERE time=(SELECT MAX(time) FROM Sensor)";
+
 $result = $conn->query($sql);
 
+//Print data for current temp
 if ($result->num_rows > 0) {
       // output data of each row
   while($row = $result->fetch_assoc()) {
@@ -29,9 +31,10 @@ if ($result->num_rows > 0) {
       else{
         $color='Grn';
       }
+      $threshTemp = $innerRow[threshold];
     }
-    $temp = $row["temp"];
-    $time = $row["time"];
+    $temp = $row[temp];
+    $time = $row[time];
     echo "<p class='temp text". $color ."'>". substr($temp, 0,2). " &degC</p>";
     echo substr($time,11,5);
   }
@@ -40,4 +43,45 @@ else {
   echo "0 results";
 }
 $conn->close();
-?>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$maxTemp = "SELECT MAX(temp), MIN(temp) FROM Sensor WHERE time > DATE_SUB(NOW(), INTERVAL 24 HOUR)";
+
+$maxResult = $conn->query($maxTemp);
+
+//Print data for max and temp of last 24 hours
+if ($maxResult->num_rows > 0) {
+      // output data of each row
+  while($row = $maxResult->fetch_assoc()) {
+
+    $tempMax = $row['MAX(temp)'];
+    $tempMin = $row['MIN(temp)'];
+
+    if (doubleval($tempMax) >= $threshTemp){
+      $colorMax = 'Red';
+    }
+    else{
+      $colorMax ='Wht';
+    }
+    if (doubleval($tempMin) >= $threshTemp){
+      $colorMin = 'Red';
+    }
+    else{
+      $colorMin ='Wht';
+    }
+
+    echo "<div class='histLbl col hist'>HIGH*</div><div class='col hist histInfo text". $colorMax ."'>". substr($tempMax, 0,2). " &degC</div><div class='col hist histLbl'>LOW*</div><div class='col hist histInfo text". $colorMin ."'>". substr($temp, 0,2). " &degC</div>";
+  }
+}
+else {
+  echo "0 results";
+}
+
+$conn->close();

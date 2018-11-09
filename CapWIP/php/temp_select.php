14a72ed2a16fdaaa4df93cadc467afd2f38bc868
berubeje <43444648+berubeje@users.noreply.github.com>
2018-11-09 13:03:49 -0500

    Update to notifications

--- CapWIP/php/temp_select.php
+++ CapWIP/php/temp_select.php
@@ -1,4 +1,13 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '/home/berubeje/public_html/Notification/PHPMailer/src/Exception.php';
+require '/home/berubeje/public_html/Notification/PHPMailer/src/PHPMailer.php';
+require '/home/berubeje/public_html/Notification/PHPMailer/src/SMTP.php';
+
+
 $servername = "142.55.32.48";
 $username = "harquaim_php";
 $password = "c@pstone_server";
@@ -26,8 +35,34 @@ if ($result->num_rows > 0) {
     $DOUBLEtemp = doubleval($row['temp']);
     while($innerRow = $thresh->fetch_assoc()){
       if ($DOUBLEtemp >= $innerRow[threshold]){
-        notification($uid, $DOUBLEtemp);
-        $color = 'Red';
+		  
+		$color = 'Red';
+		 
+		//Start of code for email notification 		 
+		$sqlEmail = "SELECT lastEmail, emailDelay FROM SENSORS WHERE uid = '".$uid."'";
+		$emailResult = $conn->query($sqlEmail);
+		
+		
+		while($innerResult = $emailResult->fetch_assoc()){
+			$lastEmail = $innerResult[lastEmail];
+			$delay = $innerResult[emailDelay];
+			//Turn time received from database to unix timestamp
+			$lastEmailUnix = strtotime($lastEmail);
+			$currentTime = time();
+			
+			//Calulated the time difference in minutes
+			$difference = ($currentTime - $lastEmailUnix)/60;			
+
+			if($difference > $delay)
+			{
+				//Send the notifiation and updated the lastemail column in the database
+			    notification($uid, $DOUBLEtemp);
+				$currentTime = date("Y-m-d H:i:s");
+				$insert = "UPDATE SENSORS SET lastEmail = '".$currentTime."' WHERE uid = '".$uid."'";
+				$runInsert = $conn->query($insert);		
+			}
+
+		}
       }
       else{
         $color='Grn';
@@ -91,17 +126,51 @@ else {
 }
 
 function notification($id, $tempera) {
-  $to = "mharquail95@hotmail.ca";
   $subject = "Sensor " . $id . " Is Over Temperature Threshold";
-
 
   $message = "Sensor ". $id . " is reading a temperature of " .substr($tempera, 0,2). " degrees celsius, which exceeds the threshold set for this sensor.";
 
+  
   $headers = 'MIME-Version: 1.0' . "\r\n";
+  $headers = 'From: Jesse Berube <berubeje@dev.fast.sheridanc.on.ca>' . "\r\n";
+  $headers .= 'Reply-To: Jesse Berube <berubeje@dev.fast.sheridanc.on.ca>' . "\r\n";
+  $headers .= 'To: <digitalmenace1@gmail.com>, <jesse@leadwave.ca>'. "\r\n";
   $headers .= 'Content-Type: text/html; charset=ISO-8859-1'. "\r\n";
-
-  mail($to,$subject,$message);
+  $headers .= 'X-Mailer: PHP/' . phpversion();
+  mail($to,$subject,$message,$headers);
+  
 }
+
+
+/*
+function notification($id, $tempera) {
+
+	
+	$mail = new PHPMailer(); // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	$mail->CharSet = 'UTF-8';
+	$mail->SMTPDebug = 4; // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true; // authentication enabled
+	$mail->Port = 587;
+	$mail->Host = 'mail.berubeje.dev.fast.sheridanc.on.ca';
+	$mail->IsHTML(true);
+	$mail->Username = "_mainaccount@berubeje.dev.fast.sheridanc.on.ca";
+	$mail->Password = "eL#*#OA91k)k";
+	$mail->SetFrom("donotreply@berubeje.dev.fast.sheridanc.on.ca", "Jesse Berube");
+	$mail->Subject = "Hello Jesse";
+	$mail->Body = "This is a test email to test PHP mailer";
+	$mail->AddAddress("jesse@leadwave.ca", "Jesse Berube");
+	$mail->AddAddress("capstoneJB@outlook.com", "Jesse Berube");
+	$mail->AddCC('digitalmenace1@gmail.com', 'Person One');
+
+	if(!$mail->Send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+		echo "Message has been sent";
+	}
+
+}
+*/
 
 $conn->close();
 

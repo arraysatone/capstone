cae03bfb8ffd5aded35a826db20acf9b10f2f2e2
berubeje <43444648+berubeje@users.noreply.github.com>
2018-11-16 12:08:16 -0500

    Update temp_select.php
    uncommented mail function

--- CapWIP/php/temp_select.php
+++ CapWIP/php/temp_select.php
@@ -25,7 +25,7 @@ $sql = "SELECT temp, time FROM SENSOR_0001203B WHERE time=(SELECT MAX(time) FROM
 
 $result = $conn->query($sql);
 
-//Print data for current temp
+//Print data for current temp WRITTEN BY JESSE
 if ($result->num_rows > 0) {
       // output data of each row
   while($row = $result->fetch_assoc()) {
@@ -39,30 +39,31 @@ if ($result->num_rows > 0) {
     		$color = 'Red';
     		 
     		//Start of code for email notification 		 
-    		//$sqlEmail = "SELECT lastEmail, emailDelay FROM EMAIL_NOTIF WHERE uid = '".$uid."'";
-    		//$emailResult = $conn->query($sqlEmail);
+    		$sqlEmail = "SELECT lastEmail, emailDelay FROM EMAIL_NOTIF WHERE uid = '".$uid."'";
+    		$emailResult = $conn->query($sqlEmail);
     		
     		
-    		// while($innerResult = $emailResult->fetch_assoc()){
-    		// 	$lastEmail = $innerResult[lastEmail];
-    		// 	$delay = $innerResult[emailDelay];
-    		// 	//Turn time received from database to unix timestamp
-    		// 	$lastEmailUnix = strtotime($lastEmail);
-    		// 	$currentTime = time();
+    		 while($innerResult = $emailResult->fetch_assoc()){
+    		 	$lastEmail = $innerResult[lastEmail];
+    		 	$delay = $innerResult[emailDelay];
+				
+    		 	//Turn time received from database to unix timestamp
+    			$lastEmailUnix = strtotime($lastEmail);
+    		 	$currentTime = time();
     			
-    		// 	//Calulated the time difference in minutes
-    		// 	$difference = ($currentTime - $lastEmailUnix)/60;			
+    		 	//Calulated the time difference in minutes
+    		 	$difference = ($currentTime - $lastEmailUnix)/60;			
 
-    		// 	if($difference > $delay)
-    		// 	{
-    		// 		//Send the notifiation and updated the lastemail column in the database
-    		// 	    notification($uid, $DOUBLEtemp);
-    		// 		$currentTime = date("Y-m-d H:i:s");
-    		// 		$insert = "UPDATE EMAIL_NOTIF SET lastEmail = '".$currentTime."' WHERE uid = '".$uid."'";
-    		// 		$runInsert = $conn->query($insert);		
-    		// 	}
+    		 	if($difference > $delay)
+    		 	{
+    		 		//Send the notifiation and updated the lastemail column in the database
+    		 	    notification($uid, $DOUBLEtemp);
+    		 		$currentTime = date("Y-m-d H:i:s");
+    		 		$insert = "UPDATE EMAIL_NOTIF SET lastEmail = '".$currentTime."' WHERE uid = '".$uid."'";
+    		 		$runInsert = $conn->query($insert);		
+    		 	}
 
-    		// }
+    		 }
       }
       else{
         $color='Grn';
@@ -125,21 +126,21 @@ else {
   echo "0 results";
 }
 
-// function notification($id, $tempera) {
-//   $subject = "Sensor " . $id . " Is Over Temperature Threshold";
+ function notification($id, $tempera) {
+   $subject = "Sensor " . $id . " Is Over Temperature Threshold";
 
-//   $message = "Sensor ". $id . " is reading a temperature of " .substr($tempera, 0,2). " degrees celsius, which exceeds the threshold set for this sensor.";
+   $message = "Sensor ". $id . " is reading a temperature of " .substr($tempera, 0,2). " degrees celsius, which exceeds the threshold set for this sensor.";
 
   
-//   $headers = 'MIME-Version: 1.0' . "\r\n";
-//   $headers = 'From: Jesse Berube <berubeje@dev.fast.sheridanc.on.ca>' . "\r\n";
-//   $headers .= 'Reply-To: Jesse Berube <berubeje@dev.fast.sheridanc.on.ca>' . "\r\n";
-//   $headers .= 'To: <digitalmenace1@gmail.com>, <jesse@leadwave.ca>'. "\r\n";
-//   $headers .= 'Content-Type: text/html; charset=ISO-8859-1'. "\r\n";
-//   $headers .= 'X-Mailer: PHP/' . phpversion();
-//   mail($to,$subject,$message,$headers);
+   $headers = 'MIME-Version: 1.0' . "\r\n";
+   $headers = 'From: <noreply@arraysatone>' . "\r\n";
+   $headers .= 'Reply-To: <noreply@arraysatone>' . "\r\n";
+   $headers .= 'To: <digitalmenace1@gmail.com>, <jesse@leadwave.ca>'. "\r\n";
+   $headers .= 'Content-Type: text/html; charset=ISO-8859-1'. "\r\n";
+   $headers .= 'X-Mailer: PHP/' . phpversion();
+   mail($to,$subject,$message,$headers);
   
-// }
+ }
 
 
 /*

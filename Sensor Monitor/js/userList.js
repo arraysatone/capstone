a0660b81caf291f165256762e548c2083194bafb
mharquail <43412943+mharquail@users.noreply.github.com>
2018-11-29 20:40:34 -0500

    fixing password changes

--- Sensor Monitor/js/userList.js
+++ Sensor Monitor/js/userList.js
@@ -155,3 +155,47 @@
 				}
 			});
 	}
+
+	function changePassword(){
+	if(confirmChange()){
+		var postData = {
+			"pass" : document.getElementById("old").value,
+			"newpass" : document.getElementById("new").value
+		};
+		$.post("php/changePass.php",postData,function(data,status){
+			console.log("Data: " + data + "\nStatus: " + status);
+			if (data == "success"){
+				console.log("Success");
+				passwordChangeSuccess();
+			}
+			else if(data == "Password Incorrect"){
+				incorrectPassword();
+			}
+		});
+	}
+}
+
+function confirmChange(){
+	if($("#new").val() == $("#newconfirm").val()){
+		console.log("Pass Match");
+		return true;
+	}
+	passwordNoMatch();
+	return false;
+}
+
+function redirect(url){
+	window.location.replace(url);
+}
+
+function incorrectPassword(){
+	alert("Password is Incorrect!");
+}
+
+function passwordNoMatch(){
+	alert("New Passwords Do Not Match!");
+}
+
+function passwordChangeSuccess(){
+	alert("Password Changed Successfully");
+}
--- Sensor Monitor/userSettings.php
+++ Sensor Monitor/userSettings.php
@@ -49,7 +49,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-		<script src="./js/passwordScript.js"></script>
+		<!--<script src="./js/passwordScript.js"></script>-->
 		<script src="./js/userList.js"></script>
 	</head>
 
